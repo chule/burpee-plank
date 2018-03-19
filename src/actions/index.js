bd0fb0efc7a7068e1ae735d362d898c13c0d9bf9
Igor Cuckovic <cuckovic@gmail.com>
2018-03-19 16:06:41 +0100

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,3 +26,16 @@ export const timerValueRemove = () => {
         type: TIMER_VALUE_REMOVE
     }
 }
+
+// let timer = null;
+// const start = () => (dispatch) => {
+//   clearInterval(timer);
+//   timer = setInterval(() => dispatch(tick()), 1000);
+//   dispatch({ type: TIMER_START });
+//   dispatch(tick())
+// }
+// const tick = () => { type: TIMER_TICK };
+// const stop = () => {
+//   clearInterval(timer);
+//   return { type: TIMER_STOP };
+// }
